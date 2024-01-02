d40d45de8123721dc3a10cfcc1702b0be32d88cb
Pixel Pioneer <pixelpioneer403@gmail.com>
2024-01-02 16:06:23 +0500

    Fixed Mobile Header

--- script.js
+++ script.js
@@ -1,92 +1,90 @@
-const wrapper = document.querySelector(".wrapper");
-const presentation = document.querySelector(".presentation");
-const mensuration = document.querySelector(".mensuration");
-const contactLink = document.querySelector(".contact-link");
-const socialmediaLink = document.querySelector(".socialmedia-link");
-const btnPopup = document.querySelector(".btnContact-popup");
-const iconClose = document.querySelector(".icon-close");
-const submitButton = document.querySelector(".btn");
-const home = document.querySelector(".home");
-const measurement = document.querySelector(".measurement");
-const galerie = document.querySelector(".galerie");
-const gallery = document.querySelector(".gallery");
-const header = document.querySelector(".header");
-const body = document.querySelector(".body");
-const mobileHeader = document.querySelector(".mobile__header");
-const mobileNavigation = document.querySelector(".mobile__navigation");
-const toggleBtn = document.querySelector(".mobile__hamburger__container img");
-let isBlurred = false;
-
-toggleBtn.addEventListener("click", () => {
-  isBlurred = !isBlurred;
-
-  mobileNavigation.style.display = isBlurred ? "flex" : "none";
-  mobileHeader.style.backdropFilter = isBlurred ? "blur(10px)" : "blur(0px)";
-});
-
-socialmediaLink.addEventListener("click", () => {
-  wrapper.classList.add("active");
-  console.log("Show Social Media");
-});
-
-contactLink.addEventListener("click", () => {
-  wrapper.classList.remove("active");
-  console.log("Show Contact");
-});
-
-btnPopup.addEventListener("click", () => {
-  wrapper.classList.add("active-popup");
-  presentation.classList.add("mask");
-  mensuration.classList.remove("show");
-  galerie.classList.remove("show");
-  body.classList.remove("gallery");
-  console.log("Show Contact");
-});
-
-iconClose.addEventListener("click", () => {
-  wrapper.classList.remove("active-popup");
-  presentation.classList.remove("mask");
-  console.log("Close Contact");
-});
-
-submitButton.addEventListener("click", () => {
-  console.log("Submit");
-});
-
-home.addEventListener("click", () => {
-  console.log("Show Home");
-  wrapper.classList.remove("active-popup");
-  presentation.classList.remove("mask");
-  mensuration.classList.remove("show");
-  galerie.classList.remove("show");
-  body.classList.remove("gallery");
-});
-
-measurement.addEventListener("click", () => {
-  console.log("Show Measurement");
-  wrapper.classList.remove("active-popup");
-  presentation.classList.add("mask");
-  mensuration.classList.add("show");
-  galerie.classList.remove("show");
-  body.classList.remove("gallery");
-});
-
-gallery.addEventListener("click", () => {
-  console.log("Show Gallery");
-  wrapper.classList.remove("active-popup");
-  presentation.classList.add("mask");
-  mensuration.classList.remove("show");
-  galerie.classList.add("show");
-  // header.classList.add("mask");
-  body.classList.add("gallery");
-});
-
-// CONTACT
-
-const inputBox = document.querySelector(".input-box");
-const label = document.querySelector(".label");
-
 $(document).ready(function () {
+  const wrapper = $(".wrapper");
+  const presentation = $(".presentation");
+  const mensuration = $(".mensuration");
+  const contactLink = $(".contact-link");
+  const socialmediaLink = $(".socialmedia-link");
+  const btnPopup = $(".btnContact-popup");
+  const iconClose = $(".icon-close");
+  const submitButton = $(".btn");
+  const home = $(".home");
+  const measurement = $(".measurement");
+  const galerie = $(".galerie");
+  const gallery = $(".gallery");
+  const header = $(".header");
+  const body = $(".body");
+  const mobileHeader = $(".mobile__header");
+  const mobileNavigation = $(".mobile__navigation");
+  const toggleBtn = $(".mobile__hamburger__container img");
+  // CONTACT
+  const inputBox = document.querySelector(".input-box");
+  const label = document.querySelector(".label");
+  let isBlurred = false;
+
+  toggleBtn.on("click", function () {
+      isBlurred = !isBlurred;
+      mobileNavigation.css("display", isBlurred ? "flex" : "none");
+      mobileHeader.css("backdropFilter", isBlurred ? "blur(10px)" : "blur(0px)");
+  });
+
+  socialmediaLink.on("click", function () {
+      wrapper.addClass("active");
+      console.log("Show Social Media");
+  });
+
+  contactLink.on("click", function () {
+      wrapper.removeClass("active");
+      console.log("Show Contact");
+  });
+
+  btnPopup.on("click", function () {
+      wrapper.addClass("active-popup");
+      presentation.addClass("mask");
+      mensuration.removeClass("show");
+      galerie.removeClass("show");
+      body.removeClass("gallery");
+      console.log("Show Contact");
+  });
+
+  iconClose.on("click", function () {
+      wrapper.removeClass("active-popup");
+      presentation.removeClass("mask");
+      console.log("Close Contact");
+  });
+
+  submitButton.on("click", function () {
+      console.log("Submit");
+  });
+
+  home.on("click", function () {
+      console.log("Show Home");
+      wrapper.removeClass("active-popup");
+      presentation.removeClass("mask");
+      mensuration.removeClass("show");
+      galerie.removeClass("show");
+      body.removeClass("gallery");
+  });
+
+  measurement.on("click", function () {
+      console.log("Show Measurement");
+      wrapper.removeClass("active-popup");
+      presentation.addClass("mask");
+      mensuration.addClass("show");
+      galerie.removeClass("show");
+      body.removeClass("gallery");
+  });
+
+  gallery.on("click", function () {
+      console.log("Show Gallery");
+      wrapper.removeClass("active-popup");
+      presentation.addClass("mask");
+      mensuration.removeClass("show");
+      galerie.addClass("show");
+      // header.addClass("mask");
+      body.addClass("gallery");
+  });
+
+
   Fancybox.bind('[data-fancybox="gallery"]');
 
   $("#contactForm").validate({
@@ -129,3 +127,4 @@ $(document).ready(function () {
     },
   });
 });
+
